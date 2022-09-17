888ea948de8476b862538524f4dcea87612dafbf
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-09-18 06:34:43 +0700

    Update start.py

--- CilikMusic/plugins/bot/start.py
+++ CilikMusic/plugins/bot/start.py
@@ -36,7 +36,7 @@ loop = asyncio.get_running_loop()
 
 
 @app.on_message(
-    filters.command(get_command("START_COMMAND"))
+    filters.command(get_command("MUSIC_COMMAND"))
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
